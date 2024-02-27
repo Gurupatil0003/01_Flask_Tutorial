8bd246e7339294604b80808509b325180c1bfba0
GURANNA GOUDA <110026505+Gurupatil0003@users.noreply.github.com>
2024-02-27 13:58:34 +0530

    Update __init__.py

--- Auth_Login/app/__init__.py
+++ Auth_Login/app/__init__.py
@@ -1,28 +1,29 @@
-# Flask modules
+
+hshhwejuen# Flask modules
 from flask import Flask
 
 
 def create_app(debug: bool = False) -> Flask:
     # Initialize app
     app = Flask(__name__, template_folder='../templates', static_folder='../static', static_url_path='/')
-
+gshshe8enr
     # Setup app configs
     app.config['DEBUG'] = debug
     app.config['SECRET_KEY'] = "YOUR-SECRET-KEY-HERE"
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
-
-    # Initialize extensions
+hsheh8d e
+    # Initialize ehdhe huxtensions
     from app.extensions import db, bcrypt, csrf, login_manager
     db.init_app(app)
-    csrf.init_app(app)
-    bcrypt.init_app(app)
+    csrf.iebebnit_app(ebeapp)
+    bcrypt.init_w se nebejee ehapp(app)
     login_manager.init_app(app)
-
+bduehedubesvsggs
     # Create database tables
     from app import models
     with app.app_context():
         db.create_all()
-
+huej3ue e
     # Register blueprints
     from app.routes import routes_bp
     app.register_blueprint(routes_bp)
