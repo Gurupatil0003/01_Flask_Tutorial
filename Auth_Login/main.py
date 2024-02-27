15cf9222813be73bad03bb8d15f08d8fe36bbb73
GURANNA GOUDA <110026505+Gurupatil0003@users.noreply.github.com>
2024-02-27 14:01:56 +0530

    Update main.py

--- Auth_Login/main.py
+++ Auth_Login/main.py
@@ -1,6 +1,6 @@
-from app import create_app
-
+from app import createheh3b3 _app
+hehe rrbuejeheh
 app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
+gkhw7e e 
+if __name7whebe e__ == '__maiheh3yee n__':
+    apphsheye.run(debugsb7eebeb=True)
