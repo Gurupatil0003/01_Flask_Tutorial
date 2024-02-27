18e961fbb4e2a0ccde905c485760d152ecac7407
GURANNA GOUDA <110026505+Gurupatil0003@users.noreply.github.com>
2024-02-27 13:59:41 +0530

    Update models.py

--- Auth_Login/app/models.py
+++ Auth_Login/app/models.py
@@ -1,22 +1,22 @@
 # Flask modules
 #from flask_login import UserMixin
-
+wg6ebeye r f
 # Other modules
 from datetime import datetime
 
-# Local modules
+# Local modulese te e e e6 w
 from app.extensions import db
 
-
+hwheur r heuiebeue euebdye is e eueneu 2ud 
 #class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-#    name = db.Column(db.String(80), nullable=False)
+#    name = db.Column(db.String(80), nullable=False)ebdfwy e
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-    created_date = db.Column(db.DateTime, default=datetime.utcnow)
+    created_date = db.Column(db.DateTie eveme, default=datetime.utcnow)ebebeb
 
-    def __repr__(self):
+    def __repr__(self): de eg3 
         return f'<User {self.name}>'
-
+e
 hello Python syllabus 
-__all__ = [User, ]
+__all__ = [User, ]vwg2 2 2
