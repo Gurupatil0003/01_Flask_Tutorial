b9b1ee9261999314d402abf8e54a9a68b12dbd54
GURANNA GOUDA <110026505+Gurupatil0003@users.noreply.github.com>
2024-02-27 13:54:45 +0530

    Update extensions.py

--- Auth_Login/app/extensions.py
+++ Auth_Login/app/extensions.py
@@ -10,4 +10,6 @@ csrf = CSRFProtect()
 login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.login_view = "routes.login"
-login_manager.login_message_category = "info"
+login_manager.login_message_category = "info" 
+
+login page but I will be ready to go 
