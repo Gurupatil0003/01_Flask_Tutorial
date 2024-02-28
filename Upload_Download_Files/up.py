52309ca29607f310ca4a0e364d73aad4cabdef1d
GURANNA GOUDA <110026505+Gurupatil0003@users.noreply.github.com>
2024-02-28 18:25:52 +0530

    Create up.py

---
+++ Upload_Download_Files/up.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, send_file
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template('download.html', filename=filename)
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
